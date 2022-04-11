7b8ddfe19ce55b9f46160bec2c627c7b2d15db3f
Estelle <estellehuynen@yahoo.fr>
2022-04-11 13:15:11 +0200

    first commit

---
+++ src/test/java/be/technifutur/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
